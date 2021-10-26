2acf50d6621239f970576b44d6381f5b497325a7
Erick Teixeira <erickhenriquemota@gmail.com>
2021-10-26 02:24:25 -0300

    Cria header básico

---
+++ components/header/index.js
@@ -0,0 +1,20 @@
+import styles from './Header.module.css';
+
+export default function Header() {
+    return(
+        <header className={styles.header}>
+            <div className={styles.headerContent}>
+                <div>
+                    <h1><a href="#">Erick Teixeira</a></h1>
+                    <h2>Ilustrador / front end developer</h2>
+                </div>
+
+                <ul className={styles.headerList}>
+                    <li><a className={styles.navItem} href="#portfolio">Portfolio</a></li>
+                    <li><a className={styles.navItem} href="#about">Sobre</a></li>
+                    <li><a className={styles.navItem} href="#contact">Contato</a></li>
+                </ul>
+            </div>
+        </header>
+    );
+}
